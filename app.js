5741dd8c3141698419cba9a184b3d8b3da55a0f0
Gregory Friel <87154134+Friel2021@users.noreply.github.com>
2021-08-30 20:20:59 -0600

    package.json console table update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const inquirer = require ("inquirer");
 // Import and require mysql2
 const mysql = require('mysql2');
-const consoleTable = require("console.table");
+const ctable = require('console.table');
 
 // Connect to database
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
 
   port: "3306",
@@ -32,30 +32,46 @@ inquirer
     message: "Please select an option",
     name: "start",
     choices: [
-      "View all departments",
-      "View all roles",
-      "View all employees",
-      "Add a department",
-      "Add a role",
-      "Add an employee",
-      "Update employee role",
-
+      "View all Departments",
+      "View all Roles",
+      "View all Employees",
+      "Add a Department",
+      "Add a Role",
+      "Add an Employee",
+      "Update Employee Role",
+      "Exit"
     ]
-
-
-
-
-
-
-
-
   }
-
-
-
-
-
 ])
+.then(function(res) {
+  switch (res.start){
 
+    case "View all Deparments":
+    viewAllDepartment();
+    break;
 
+    case "View all Roles":
+    viewAllRoles();
+    break;
+
+    case "View all Employees":
+    viewAllEmployees();
+    break;
+
+    case "Add a Department":
+    addDepartment();
+    break;
+
+    case "Add a Role":
+    addRole();
+    break;
+
+    case "Add an Employee":
+    addEmployee();
+    break;
+
+    case "Update Employee Role":
+    updateEmployeeRole();
+    break;
+  }
 }
